5f8d123744468f8a4688abbe9075a0a78f300a91
Thiago <thsethub@gmail.com>
2025-05-02 17:59:49 -0300

    .

--- src/app/sing-up.tsx
+++ src/app/sing-up.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe";
+import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { router } from "expo-router";
 
